9afad02de12ed7691580de68b6fedd14c8805fa5
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-03-01 12:38:11 +0530

    Added Constructors, Gettes, Settes and Methods in Ticket class

--- app/src/main/java/org/example/entities/Ticket.java
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -11,9 +11,81 @@ public class Ticket {
     private Date dateOfTravel;
     private Train train;
 
-    // Methods
 
+    // Constructor
+    public Ticket(String ticketId, String userId, String source, String destination, Date dateOfBooking, Date dateOfTravel, Train train) {
+        this.ticketId = ticketId;
+        this.userId = userId;
+        this.source = source;
+        this.destination = destination;
+        this.dateOfBooking = dateOfBooking;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
+    }
+
+
+    // Methods
     public String getTicketInfo(){
         return String.format("Ticket ID: %s belongs to User %s from %s to %s on %s", ticketId, userId, source, destination, dateOfTravel);
+    }
+
+
+    // Getters
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public Date getDateOfBooking() {
+        return dateOfBooking;
+    }
+
+    public Date getDateOfTravel() {
+        return dateOfTravel;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+
+    // Setters
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setDateOfBooking(Date dateOfBooking) {
+        this.dateOfBooking = dateOfBooking;
+    }
+
+    public void setDateOfTravel(Date dateOfTravel) {
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
     }
 }
