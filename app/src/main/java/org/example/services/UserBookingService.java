a4487fb20a2783b508099f4f1b15d3e8bcd946c5
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-03-01 15:23:39 +0530

    Added UserServiceUtil class and Updated UserBookingService class

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -3,10 +3,11 @@ package org.example.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.entities.User;
+import org.example.util.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 public class UserBookingService {
     private User user; // Storing user at global level
@@ -16,9 +17,38 @@ public class UserBookingService {
 
     private ObjectMapper objectMapper = new ObjectMapper(); // Jackson's ObjectMapper to map values to classes
 
+
+    // Constructor
     public UserBookingService(User u) throws IOException {
         this.user = u;
         File users = new File(USER_PATH);
         userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
     }
+
+    // Methods
+
+    // LOGIN
+    public boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter( user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+                }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    // SIGNUP
+    public boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException{
+        File usersFile = new File(USER_PATH);
+        objectMapper.writeValue(usersFile, userList);
+    }
+
 }
